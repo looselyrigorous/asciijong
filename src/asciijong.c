42f2096a78f9112032753151d879614551a4ffda
George Kaklamanos <gkaklas1@gmail.com>
2015-06-09 21:58:28 +0300

    lalala

--- src/asciijong.c
+++ src/asciijong.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ui.h"
+#include "asciijong.h"
+
 int main(int argc, const char **argv){
+    board_t board;
+    tile_t tile0,tile1;
+    board = readBoardFromFile( (char *)argv[1] );
+    while(1){
+        refreshBoard( *board, *hl);
+        tile0 = getTileFromUser( void );
+        validateTileFromUser( *tile0 );
+        while ( tilegetTilesLike( *tile0 ) ){
+
+        }
+
+
+
+    }
+    
     return 0;
 }
