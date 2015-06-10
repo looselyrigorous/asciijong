d1f064e246f98db490f2e6b74e0c5a2b4a0be615
looselyrigorous <looselyrigorous@gmail.com>
2015-06-10 18:17:47 +0300

    add basic 2d tile printing

--- src/ui.c
+++ src/ui.c
@@ -3,19 +3,97 @@
  * author: looselyrigorous
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "ui.h"
+
 #include "types.h"
 
-tile_t *getTileFromUser(void) {
-	double x,y;
-	tile_t tile;
+#define ever (;;)
 
-	printf("Enter two doubles separated by comma: ");
-	scanf("%lf,%lf", &x, &y);
-
-	tile = getTileAt(x,y);
-	return tile;
+void showErrorMessageToUser(const char *errormsg) {
+	printf("%s\n", errormsg);
+	return;
 }
 
-void refreshBoard(board_t *b, tile_t hl) {
+tile_t *getTileFromUser(board_t *b) {
+	coordt_t x=0.0, y=0.0;
+	tile_t tile;
+	for ever {
+		printf("\nEnter tile coordinates separated by comma: ");
+		scanf("%lf,%lf", &x, &y);
+		tile = validateTileFromUser(b,x,y);
+		if(tile != NULL) return tile;
+		showErrorMessageToUser("Invalid coordinates.");
+	}
+}
+
+void refreshBoard(board_t *b, tile_t *hl) {
+	coordt_t x=0.0, y=0.0;
+	int tmp=0;
+
+	dimensions_t dim;
+	tile_t tile, tile_up, tile_down;
+	char *l1=NULL, *l2=NULL, *l3=NULL,
+		 *l4=NULL, *l5=NULL;
+
+	dim = getBoardDimensions(dim);
 	
+	/* Memory allocation */
+	tmp = (int) getMaximumX(dim);
+	l1 = malloc(sizeof(char) * tmp);
+	l2 = malloc(sizeof(char) * tmp);
+	l3 = malloc(sizeof(char) * tmp);
+	l4 = malloc(sizeof(char) * tmp);
+	l5 = malloc(sizeof(char) * tmp);
+
+	/* Refresh sequence */
+	printf("\e[1;1H\e[2J"); /* Clears screen */
+	for(y=getMinimumY(dim); y<getMaximumY(dim); y+=0.5) {
+		for(x=getMinimumX(dim); x<getMaximumX(dim); x+=0.5) {
+			tile = getTileAt(b,x,y);
+			if(fmod(y, 1.0) == 0.0) {
+				if(y-0.5>getMinimumY(dim)) tile_up = getTileAt(b,x,y-0.5);
+				if(y+0.5<getMaximumY(dim)) tile_down = getTileAt(b,x,y+0.5);
+			}
+
+			if(tile != NULL) {
+				l1 = strcat(l1, "+---+");
+				l2 = strcat(l2, "|   |");
+
+				l3 = strcat(l3, "| ");
+				l3 = strcat(l3, getTileName(tile));
+				l3 = strcat(l3, " |");
+				
+				l4 = strcat(l4, "|   |");
+				l5 = strcat(l5, "+---+");
+			} else {
+				if(tile_up != NULL) {
+					l1 = strcat(l1, "| ");
+					l1 = strcat(l1, getTileName(tile));
+					l1 = strcat(l1, " |");
+					l2 = strcat(l2, "|   |");
+					l3 = strcat(l3, "+---+");
+				} else {
+					l1 = strcat(l1, "     ");
+					l2 = strcat(l2, "     ");
+					l3 = strcat(l3, "     ");
+				}
+
+				else if(tile_down != NULL) {
+					l4 = strcat(l4, "+---+");
+					l5 = strcat(l5, "|   |");
+				} else {
+					l4 = strcat(l4, "     ");
+					l5 = strcat(l5, "     ");
+				}
+			}
+		}
+		printf("%s\n",l1);
+		printf("%s\n",l2);
+		printf("%s\n",l3);
+		printf("%s\n",l4);
+		printf("%s\n",l5);
+	}
 }
--- src/ui.h
+++ src/ui.h
@@ -2,9 +2,17 @@
  * file: ui.h
  * author: looselyrigorous
  */
+#include "types.h"
 
-tile_t *getTileFromUser(void);
+/* Asks the user to select a valid tile, then returns it.
+ * Always returns valid tile from board *b */
+tile_t *getTileFromUser(board_t *b);
 
-void refreshBoard(board_t *b, tile_t hl);
+/* Refreshes board *b, and highlights tile *hl
+ * Called after the program has received input
+ * and acted according to it*/
+void refreshBoard(board_t *b, tile_t *hl);
 
+/* Shows an error message to the user.
+ * Should be used for errors related to the game itself. */
 void showErrorMessageToUser(const char *errormsg);
