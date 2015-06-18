05d868848fa4588e9369213b0521ecff1173b0cf
George Kaklamanos <gkaklas1@gmail.com>
2015-06-19 01:45:48 +0300

    lalala1

--- src/asciijong.c
+++ src/asciijong.c
@@ -8,11 +8,23 @@ int main(int argc, const char **argv){
     tile_t tile0,tile1;
     board = readBoardFromFile( (char *)argv[1] );
     while(1){
-        refreshBoard( *board, *hl);
+        /* Print Board */
+        refreshBoard( *board, NULL);
+        /* Accept input from the user */
         tile0 = getTileFromUser( void );
         validateTileFromUser( *tile0 );
-        while ( tilegetTilesLike( *tile0 ) ){
-
+        /* If the user selected a valid tile */
+        if ( canRemoveTile( *tile0 ) ){
+            /* Redraw the board, this time highlighting the selected tile */
+            refreshBoard( *board, *tile0 );
+            tile1 = getTileFromUser( void );
+            if ( tilesMatch( *tile0, *tile1 ) ){
+                rmTile( *board, *tile0, *tile1);
+            }
+            continue;
+        /* If the user selected an invalid tile print an error */
+        } else {
+            showErrorMessageToUser("This tile can't be remove. Please select another one. :)");
         }
 
 
