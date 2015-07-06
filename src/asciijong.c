58b7e31b0cc8bf0c5502bf0ffdc0b68ce7114015
George Kaklamanos <gkaklas1@gmail.com>
2015-07-06 09:43:47 +0300

    moar code

--- src/asciijong.c
+++ src/asciijong.c
@@ -7,29 +7,39 @@ int main(int argc, const char **argv){
     board_t board;
     tile_t tile0,tile1;
     board = readBoardFromFile( (char *)argv[1] );
+    if(!board){
+        showErrorMessageToUser("Could not read board data from file: %s", (char *)argv[1]);
+        exit(-1);
+    }
     while(1){
         /* Print Board */
         refreshBoard( *board, NULL);
-        /* Accept input from the user */
-        tile0 = getTileFromUser( void );
-        validateTileFromUser( *tile0 );
-        /* If the user selected a valid tile */
-        if ( canRemoveTile( *tile0 ) ){
-            /* Redraw the board, this time highlighting the selected tile */
-            refreshBoard( *board, *tile0 );
-            tile1 = getTileFromUser( void );
-            if ( tilesMatch( *tile0, *tile1 ) ){
-                rmTile( *board, *tile0, *tile1);
+        do{
+            tile0 = getTileFromUser( *board );
+        }while( !tile0 );
+        if ( canRemoveTile( *tile0) ){
+            /* Redraw the board, this time highlighting the selected tile. */
+            refreshBoard( *board, *tile0);
+            tile1 = getTileFromUser( *board );
+            while( !tilesMatch( *tile0, *tile1 ) && !canRemoveTile(*tile1) ){
+                if( !tilesMatch( *tile0, *tile1 ) ){
+                    showErrorMessageToUser("Tile %c does not match %c." TileValues[tile1.ttype], TileValues[tile1.ttype]);
+                }
+                if( !canRemoveTile(*tile1) ){
+                    showErrorMessageToUser("Tile %c cannot be removed." TileValues[tile1.ttype]);
+                }
+                tile1 = getTileFromUser( *board );
             }
-            continue;
-        /* If the user selected an invalid tile print an error */
+            /* The user may deselect the first tile and choose a whole new pair. */
+            if( tile1 == tile0 ){
+                continue;
+            }
+            rmTile( *board, *tile0);
+            rmTile( *board, *tile1);
         } else {
-            showErrorMessageToUser("This tile can't be remove. Please select another one. :)");
+            /* Print an error if the user selected an invalid tile*/
+            showErrorMessageToUser("This tile cannot be removed.");
         }
-
-
-
     }
-    
     return 0;
 }
