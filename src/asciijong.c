a4e8c325cbb54e64f07ca27645fc7b1a1e647684
George Kaklamanos <gkaklas1@gmail.com>
2015-07-06 10:14:53 +0300

    Corrected the pointer magic.

--- src/asciijong.c
+++ src/asciijong.c
@@ -4,8 +4,8 @@
 #include "asciijong.h"
 
 int main(int argc, const char **argv){
-    board_t board;
-    tile_t tile0,tile1;
+    board_t *board;
+    tile_t *tile0,*tile1;
     board = readBoardFromFile( (char *)argv[1] );
     if(!board){
         showErrorMessageToUser("Could not read board data from file: %s", (char *)argv[1]);
@@ -13,29 +13,29 @@ int main(int argc, const char **argv){
     }
     while(1){
         /* Print Board */
-        refreshBoard( *board, NULL);
+        refreshBoard( &board, NULL);
         do{
-            tile0 = getTileFromUser( *board );
+            tile0 = getTileFromUser( &board );
         }while( !tile0 );
-        if ( canRemoveTile( *tile0) ){
+        if ( canRemoveTile( &tile0) ){
             /* Redraw the board, this time highlighting the selected tile. */
-            refreshBoard( *board, *tile0);
-            tile1 = getTileFromUser( *board );
-            while( !tilesMatch( *tile0, *tile1 ) && !canRemoveTile(*tile1) ){
-                if( !tilesMatch( *tile0, *tile1 ) ){
-                    showErrorMessageToUser("Tile %c does not match %c." TileValues[tile1.ttype], TileValues[tile1.ttype]);
+            refreshBoard( &board, &tile0);
+            tile1 = getTileFromUser( &board );
+            while( !tilesMatch( &tile0, &tile1) && !canRemoveTile( &tile1) ){
+                if( !tilesMatch( &tile0, &tile1) ){
+                    showErrorMessageToUser("Tile %c does not match %c.", TileValues[tile1->ttype], TileValues[tile1->ttype]);
                 }
-                if( !canRemoveTile(*tile1) ){
-                    showErrorMessageToUser("Tile %c cannot be removed." TileValues[tile1.ttype]);
+                if( !canRemoveTile(&tile1) ){
+                    showErrorMessageToUser("Tile %c cannot be removed.", TileValues[tile1->ttype]);
                 }
-                tile1 = getTileFromUser( *board );
+                tile1 = getTileFromUser( &board);
             }
             /* The user may deselect the first tile and choose a whole new pair. */
-            if( tile1 == tile0 ){
+            if( *tile1 == *tile0 ){
                 continue;
             }
-            rmTile( *board, *tile0);
-            rmTile( *board, *tile1);
+            rmTile( &board, &tile0);
+            rmTile( &board, &tile1);
         } else {
             /* Print an error if the user selected an invalid tile*/
             showErrorMessageToUser("This tile cannot be removed.");
