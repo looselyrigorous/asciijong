69b30b8ae8ecedd62cf49c81eb1230589e70b06b
George Kaklamanos <gkaklas1@gmail.com>
2015-07-11 00:26:05 +0300

    Fixed wrong parameters while calling functions

--- src/asciijong.c
+++ src/asciijong.c
@@ -24,15 +24,15 @@ int main(int argc, const char **argv){
         do{
             tile0 = getTileFromUser( &board );
         }while( !tile0 );
-        if ( canRemoveTile( &tile0) ){
+        if ( canRemoveTile( &board, &tile0) ){
             /* Redraw the board, this time highlighting the selected tile. */
             refreshBoard( &board, &tile0);
             tile1 = getTileFromUser( &board );
-            while( !tilesMatch( &tile0, &tile1) && !canRemoveTile( &tile1) ){
-                if( !tilesMatch( &tile0, &tile1) ){
+            while( !tilesMatch( &board, &tile0, &tile1) && !canRemoveTile( &board, &tile1) ){
+                if( !tilesMatch( &board, &tile0, &tile1) ){
                     showErrorMessageToUser("Tiles do not match.");
                 }
-                if( !canRemoveTile(&tile1) ){
+                if( !canRemoveTile( &board, &tile1) ){
                     showErrorMessageToUser("This tile cannot be removed.");
                 }
                 tile1 = getTileFromUser( &board);
